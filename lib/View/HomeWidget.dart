076c1a101e7202ccbb6e8aaa064ccc32d8752a98
xxminamixx <pencil3215@gmail.com>
2021-01-19 14:54:32 +0900

    test

--- lib/View/HomeWidget.dart
+++ lib/View/HomeWidget.dart
@@ -16,7 +16,7 @@ class HomeWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Screen(context);
-
+    print('hoge');
     return Scaffold(
       body: Center(
         child: Column(
